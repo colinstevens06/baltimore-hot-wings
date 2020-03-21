54ccc68760f244511438bab62067768cf007d1c0
Colin Stevens <colinstevens06@gmail.com>
2020-03-21 00:51:09 -0400

    isSpecial now doing a little something

--- client/src/components/Restaurant-Cards/index.js
+++ client/src/components/Restaurant-Cards/index.js
@@ -7,7 +7,9 @@ import "./style.css"
 function RestaurantCard(props) {
   return (
     <div>
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }}
+        className={props.isSpecial ? ("isSpecial") : ("")}
+      >
         <Card.Body>
           <Card.Title>{props.name}</Card.Title>
           <Card.Subtitle className="mb-2">{props.neighborhood}</Card.Subtitle>
