3ccc234dbbd56541521927d31b05508b6233a53b
Colin Stevens <colinstevens06@gmail.com>
2020-03-22 17:52:05 -0400

    navigation styled and linked and also did some updates to the landing page styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import WingsLogo from '../../assets/images/logos/bmore-wings-logo-small.png'
 
 // NAVBAR
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function Navigation() {
   return (
     <div>
 
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">BMore Wing App</Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" variant="dark">
+        <img className="img-fluid nav-image" src={WingsLogo} alt="BMore Wings Logo" />
+        <Navbar.Brand href="/">BMore Wing App</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Restaurants</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#restaurants">Restaurants</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- client/src/pages/Individual.js
+++ client/src/pages/Individual.js
@@ -8,35 +8,15 @@ import IndividualHero from "../components/Hero-Individual/index.js";
 import StoreInfo from "../components/Table-Individual"
 
 function IndividualPage() {
-  // getting the id from the URL
-  // const { id } = useParams()
-
-  // const [restaurant, setRestaurant] = useState({})
-
-  // // using that ID to set the state for the retaurant's information
-  // useEffect(() => {
-  //   API.getRestaurant(id)
-  //     // .then(res => console.log(res.data))
-  //     .then(res => setRestaurant(res.data))
-  //     .catch(err => console.log(err))
-  // }, [])
 
   return (
     <div>
       <Container>
-        <IndividualHero
-        // name={restaurant.name}
-        // id={restaurant._id}
-        // city={restaurant.location.city.name}
-        // cityName={restaurant.location}
-        />
+        <IndividualHero />
         <StoreInfo />
-
-
       </Container>
     </div>
   )
 }
-
 
 export default IndividualPage
