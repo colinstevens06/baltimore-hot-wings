23794d163a1e6269ce20245890ec30da16b30a29
Colin Stevens <colinstevens06@gmail.com>
2020-03-22 21:16:06 -0400

    switched to table and isSpecial has a reaction now

--- client/src/components/Table-Individual/index.js
+++ client/src/components/Table-Individual/index.js
@@ -5,14 +5,18 @@ import { useParams } from "react-router-dom";
 
 // layout
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Table from 'react-bootstrap/Table'
+
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDrumstickBite } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function StoreInfo() {
   const { id } = useParams()
 
   const [store, setStore] = useState(undefined)
-  const [today, setToday] = useState(undefined)
 
   // using that ID to set the state for the retaurant's information
   useEffect(() => {
@@ -22,124 +26,66 @@ function StoreInfo() {
       .catch(err => console.log(err))
   }, [])
 
-  useEffect(() => {
-    const today = new Date().getDay();
-    setToday(today)
-  }, [])
-
   return (
     <div>
       <Container>
-        <Row>
-          <Col></Col>
-          <Col>
-            <h3>Pricing</h3>
-          </Col>
-          <Col>
-            <h3>Hours</h3>
-          </Col>
-        </Row >
-        {store &&
-          <div>
+        <Table striped>
+          <thead>
+            <tr>
+              <th className="day-of-week"></th>
+              <th>Pricing</th>
+              <th>Hours</th>
+            </tr>
+          </thead>
 
 
+          {store &&
 
-            
+            <tbody>
+              <tr>
+                <td className="day-of-week">{store.location.hours[0].day}</td>
+                <td>${store.wings[0].price} for {store.wings[0].count} {store.wings[0].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[0].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[1].day}</td>
+                <td>${store.wings[1].price} for {store.wings[1].count} {store.wings[1].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[1].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[2].day}</td>
+                <td>${store.wings[2].price} for {store.wings[2].count} {store.wings[2].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[2].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[3].day}</td>
+                <td>${store.wings[3].price} for {store.wings[3].count} {store.wings[3].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[3].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[4].day}</td>
+                <td>${store.wings[4].price} for {store.wings[4].count} {store.wings[4].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[4].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[5].day}</td>
+                <td>${store.wings[5].price} for {store.wings[5].count} {store.wings[5].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[5].time}</td>
+              </tr>
+              <tr>
+                <td className="day-of-week">{store.location.hours[6].day}</td>
+                <td>${store.wings[6].price} for {store.wings[6].count} {store.wings[6].isSpecial ? <FontAwesomeIcon icon={faDrumstickBite} /> : ""}</td>
+                <td>{store.location.hours[6].time}</td>
+              </tr>
 
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[0].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[0].price} for {store.wings[0].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[0].time}</p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[1].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[1].price} for {store.wings[1].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[1].time}</p>
-              </Col>
-            </Row>
-            <hr />
+            </tbody>
 
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[2].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[2].price} for {store.wings[2].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[2].time}</p>
-              </Col>
-            </Row>
-            <hr />
+          }
+        </Table>
 
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[3].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[3].price} for {store.wings[3].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[3].time}</p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[4].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[4].price} for {store.wings[4].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[4].time}</p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[5].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[5].price} for {store.wings[5].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[5].time}</p>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col className="day-of-week">
-                <p><strong>{store.location.hours[6].day}</strong></p>
-              </Col>
-              <Col>
-                <p>${store.wings[6].price} for {store.wings[6].count}</p>
-              </Col>
-              <Col>
-                <p>{store.location.hours[6].time}</p>
-              </Col>
-            </Row>
-
-
-
-          </div>
-
-        }
 
       </Container>
-    </div >
+    </div>
   )
 }
 
--- client/src/pages/Individual.js
+++ client/src/pages/Individual.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-// import API from "../utils/API.js";
-// import { useParams } from "react-router-dom";
+import React from "react";
 
 // layout features
 import Container from 'react-bootstrap/Container';
